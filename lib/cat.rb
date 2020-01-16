f7232f4f965cb9db185f414443449b6699cbac38
Charlie Oster <oster.charlie@yahoo.com>
2020-01-16 02:50:24 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
-class Cat
+lass Cat
   # code goes here
+end 
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @mood = mood
+    @name = name
+  end
 end
