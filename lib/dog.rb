159fa60a0ad3c7b7e89d9dc622e8bf8e302dd373
Charlie Oster <oster.charlie@yahoo.com>
2020-01-16 02:51:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,9 @@
-lass Dog
-  # code goes here
-  attr_reader :name
+class Dog
   attr_accessor :mood
+  attr_reader :name
 
-  def initialize(name, mood = "nervous")
-    @mood = mood
+  def initialize(name)
     @name = name
+    @mood = "nervous"
   end
-end 
+end
